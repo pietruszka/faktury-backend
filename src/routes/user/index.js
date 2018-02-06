61b46ac5bdeac17d25b30208ee72cfff954ceb12
Piotrek <piotr.pietruszka@o2.pl>
2018-02-06 13:39:36 +0100

    rewrite user settings

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,17 +1,8 @@
 const router = require("express").Router;
-const authCheck = require("./../authMiddleware");
-const {
-    getUser,
-    addUser,
-    removeUser,
-} = require("./controller");
 
 class UserRouter {
     constructor() {
         this.router = router();
-        this.router.get("/", authCheck, getUser);
-        this.router.post("/", authCheck, addUser);
-        this.router.delete("/:id", authCheck, removeUser);
     }
 
     getRouter() {
